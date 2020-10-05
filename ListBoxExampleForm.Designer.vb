760f6305eaa982afbb1cca9069b3804b829b0170
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2020-10-05 08:19:57 -0600

    Add project files.

---
+++ ListBoxExampleForm.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ListBoxExampleForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DisplayListBox = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'DisplayListBox
+        '
+        Me.DisplayListBox.FormattingEnabled = True
+        Me.DisplayListBox.ItemHeight = 20
+        Me.DisplayListBox.Location = New System.Drawing.Point(249, 12)
+        Me.DisplayListBox.Name = "DisplayListBox"
+        Me.DisplayListBox.Size = New System.Drawing.Size(539, 324)
+        Me.DisplayListBox.TabIndex = 0
+        '
+        'ListBoxExampleForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.DisplayListBox)
+        Me.Name = "ListBoxExampleForm"
+        Me.Text = "List Box Example"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DisplayListBox As ListBox
+End Class
---
+++ ListBoxExampleForm.vb
@@ -0,0 +1,12 @@
+﻿Public Class ListBoxExampleForm
+    Private Sub ListBoxExampleForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        UpdateListBox()
+    End Sub
+
+    Sub UpdateListBox()
+
+
+
+    End Sub
+
+End Class
